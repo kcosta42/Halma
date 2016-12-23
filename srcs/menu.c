85ef94b7f558f1c4d74655ea3880c634c207db84
Kevin Costa <kcosta@Kevins-MacBook-Pro.local>
2016-12-23 21:54:52 +0100

    Commit

--- srcs/menu.c
+++ srcs/menu.c
@@ -6,7 +6,7 @@
 /*   By: kcosta <kcosta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 00:21:48 by kcosta            #+#    #+#             */
-/*   Updated: 2016/11/17 15:06:30 by kcosta           ###   ########.fr       */
+/*   Updated: 2016/12/20 22:15:47 by kcosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,14 @@ int		display_menu(void)
 
 	nb_player = 0;
 	system("clear");
-	printf("\n");
-	printf("\n");
-	printf("  ##  ##       ##     ##       ##     ##        ##\n");
-	printf("  ##  ##     ##  ##   ##      ## ## ## ##     ##  ##\n");
-	printf("  ######    ##   ##   ##      ##   ##  ##    ##   ##\n");
-	printf("  ##  ##   ## ### ##  ##      ##       ##   ## ### ##\n");
-	printf("  ##  ##  ##      ##  ######  ##       ##  ##      ##\n");
-	printf("\n");
-	printf("\n");
-	printf("Choose number of player (2 or 4): \n");
-	while (nb_player != 2 && nb_player != 4)
+	printf("\n\n");
+	printf("   / / / /   |  / /   /  |/  /   |\n");
+	printf("  / /_/ / /| | / /   / /|_/ / /| |\n");
+	printf(" / __  / ___ |/ /___/ /  / / ___ |\n");
+	printf("/_/ /_/_/  |_/_____/_/  /_/_/  |_|\n");
+	printf("\n\n");
+	printf("Choose number of player (2 to 4): \n");
+	while (nb_player < 2 || nb_player > 4)
 	{
 		//scanf("%d", &nb_player);
 		nb_player = getchar();
